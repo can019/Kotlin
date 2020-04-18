d9715ceb1d15a4836233fef2a53874a3cb0b7298
can019 <jys01012@gmail.com>
2020-04-19 00:57:59 +0900

    add :: permission Test

--- Android/App2_JYS/app/src/main/java/com/example/app2_jys/Search.kt
+++ Android/App2_JYS/app/src/main/java/com/example/app2_jys/Search.kt
@@ -1,4 +1,11 @@
 package com.example.app2_jys
 
+
+import android.os.Environment
+
+
 class Search {
+    private val EXTERNAL_STORAGE_PATH = Environment.getExternalStorageState().toString()
+    fun find(){
+    }
 }
