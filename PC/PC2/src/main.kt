306c0a7c7956e368279ea392274bce126015f3bb
can019 <jys01012@gmail.com>
2020-04-19 01:12:10 +0900

    Add :: Init,Constructor method

--- PC/PC2/src/main.kt
+++ PC/PC2/src/main.kt
@@ -1,6 +1,15 @@
 fun main(){
 
     var a = Person("박보영", 1990)
+    var b = Person("정유성")
     println("안녕하세요 ${a.name} 입니다")
 }
-class Person (var name:String, var birthYear:Int)
+class Person (var name:String, var birthYear:Int){
+    init {
+        println("생성되었습니다.")
+    }
+
+    constructor(name:String) :this(name,1997){
+        println("보조생성자가 사용되었습니다.")
+    }
+}
