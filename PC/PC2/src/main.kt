4e06f2ad0af8374dfd158c06e572b74179d4092a
can019 <jys01012@gmail.com>
2020-04-19 01:19:01 +0900

    inherit add

--- PC/PC2/src/main.kt
+++ PC/PC2/src/main.kt
@@ -2,6 +2,10 @@ fun main(){
 
     var a = Person("박보영", 1990)
     var b = Person("정유성")
+    var c = Animal("멍멍",13,"개")
+    var d = Dog("별이", 5)
+    c.introduce()
+    d.introduce()
     println("안녕하세요 ${a.name} 입니다")
 }
 class Person (var name:String, var birthYear:Int){
@@ -13,3 +17,9 @@ class Person (var name:String, var birthYear:Int){
         println("보조생성자가 사용되었습니다.")
     }
 }
+open class Animal (var name:String, var age:Int, var type:String){
+    fun introduce(){
+        println("저는 ${type} ${name}이고, ${age}살 입니다.")
+    }
+}
+class Dog(name:String,age:Int) : Animal(name,age,"개")
